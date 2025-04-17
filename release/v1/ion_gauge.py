dd536b6a5d88fbd3671966994f10bc2060425f98
yaorui <yaorui1996@gmail.com>
2025-04-17 13:11:26 +0800

    更新iongauge

--- release/v1/ion_gauge.py
+++ release/v1/ion_gauge.py
@@ -1,6 +1,11 @@
 import re
+import os
+import csv
+import time
 import serial
 import serial.tools.list_ports
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def find_com_name(serial_number:str) -> str:
@@ -14,8 +19,14 @@ def find_com_name(serial_number:str) -> str:
 
 class IonGauge:
     
-    def __init__(self, serial_number:str, baudrate=9600) -> None:
-        self.__ser = serial.Serial(port=find_com_name(serial_number), baudrate=baudrate)
+    def __init__(self, serial_number:str=None, com:str=None, baudrate=9600) -> None:
+        if serial_number:
+            port = find_com_name(serial_number)
+        elif com:
+            port = com
+        else:
+            raise
+        self.__ser = serial.Serial(port=port, baudrate=baudrate)
         self.__ser.timeout = 0.5
 
     def close(self) -> None:
@@ -23,7 +34,7 @@ class IonGauge:
     
     def query(self, cmd:str) -> str:
         self.__ser.write(f'{cmd}\r'.encode())
-        recv = self.__ser.read(1024).decode()
+        recv = self.__ser.readline().decode()
         res = re.compile(r'>(\S+)\r').findall(recv)
         return res[0] if res else ''
         
@@ -41,11 +52,36 @@ class IonGauge:
         
     def set_fil_2_emission_on(self) -> None:
         self.query('#0033UHFIG1')
-        
+
+
+def query(inst:IonGauge) -> None:
+    t_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    p = inst.read_pressure()
+    new_row = [t_now, p]
+    print(new_row)
+    with open(data_file, 'a', newline='', encoding='utf-8') as file:
+        csv.writer(file).writerow(new_row)
+    
 
 if __name__ == '__main__':
-    inst = IonGauge('AYDPE11BS13')
-    print(inst.read_emission_status())
-    print(inst.read_pressure())
-    inst.close()
+    
+    data_file = '_data_IonGauge.csv'
 
+    if not os.path.exists(data_file):
+        with open(data_file, 'w', newline='', encoding='utf-8') as file:
+            csv.writer(file).writerow(['Time', 'Pressure(Pa)'])
+    
+    inst = IonGauge(serial_number='AYDPE11BS13')
+    
+    next_whole_second = (datetime.now() + timedelta(seconds=1)).replace(microsecond=0)
+    sched = BackgroundScheduler()
+    sched.add_job(query, 'interval', seconds=1, start_date=next_whole_second, args=[inst])
+    sched.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        sched.shutdown(wait=True)
+        inst.close()
+        print("\n检测到 Ctrl+C，程序退出。")
